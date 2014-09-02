7f4d38805bc77306233abb75ccaba4d30c9760dd
Connor Peet <connor@connorpeet.com>
2014-09-02 19:30:21 -0400

    Completed exercise 1

---
+++ exercise-1/error_library.py
@@ -0,0 +1,47 @@
+# Exercise 1, Task 1: Runtime Errors
+#
+# CSC148 Fall 2014, University of Toronto
+# Instructor: David Liu
+# ---------------------------------------------
+# STUDENT INFORMATION
+#
+# List your information below, in format
+# Connor Peet, 1001088208
+#
+# ---------------------------------------------
+"""Exercise 1, Task 1: Runtime Errors."""
+
+
+def bad_type():
+    """When run, produces a TypeError."""
+    "This isn't Javascript... " + 42
+
+
+def bad_name():
+    """When run, produces a NameError."""
+    foo
+
+
+def bad_attribute():
+    """When run, produces an AttributeError."""
+    'this is a bad'.attribute
+
+
+def bad_index():
+    """When run, produces an IndexError."""
+    [][42]
+
+
+def bad_key():
+    """When run, produces a KeyError."""
+    {}['bar']
+
+
+def bad_zero():
+    """When run, produces a ZeroDivisionError."""
+    42 / 0
+
+
+def bad_import():
+    """When run, produces an ImportError."""
+    import someSillyModule
